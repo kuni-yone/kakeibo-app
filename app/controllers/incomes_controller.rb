9c25d745b39586cc312ab5a07f989c5bf0eee987
kunihiko-yonenaga <rabessi.coo@gmail.com>
2020-01-30 23:18:43 +0900

    モデル、コントローラの作成

---
+++ app/controllers/incomes_controller.rb
@@ -0,0 +1,24 @@
+class IncomesController < ApplicationController
+  def index
+    @incomes = Income.order(created_at: :asc)
+  end
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+end
